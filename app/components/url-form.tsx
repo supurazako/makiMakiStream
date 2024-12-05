2c9a10b7159292333a4a09ee02a404a85124880d
supurazako <redhot30atama@gmail.com>
2024-12-05 16:58:40 +0900

    feat(twitch): convert alert into twitch-stream-box

--- app/components/url-form.tsx
+++ app/components/url-form.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TwitchStreamBox from './twitch-stream-box';
 
 export default function UrlForm() {
+    const [streamUrl, setStreamUrl] = useState<string | null>(null);
+
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const data = new FormData(event.target as HTMLFormElement);
-        alert(data.get('inputText'));
+        // TODO: Add stream box
+        // Check start with https://www.twitch.tv/
+        const inputText = data.get('inputText') as string;
+
+        if (inputText.startsWith('https://www.twitch.tv/')) {
+            // Formatting sentences into embedding URL
+            const formattedText = inputText.replace('https://www.twitch.tv/', 'https://player.twitch.tv/?channel=');
+            // Add stream box
+            setStreamUrl(formattedText);
+        } else {
+            alert('Invalid URL, plaese enter a valid Twitch URL');
+        }
     }
     return (
-        <form className='border-4 border-black' onSubmit={handleSubmit}>
-            <input type="text" name="inputText" />
-            <button type="submit">Submit</button>
-        </form>
+        <>
+            <form className='border-4 border-black' onSubmit={handleSubmit}>
+                <input type="text" name="inputText" />
+                {/* TODO: submitをonClickイベントにする */}
+                <button type="submit">Submit</button>
+            </form>
+            {streamUrl && <TwitchStreamBox url={streamUrl} />}
+        </>
     )
 }
