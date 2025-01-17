f607326211e1dd6d5dd143396681a92971b8b87e
Zemelua <zemelua1107@gmail.com>
2025-01-17 13:03:18 +0900

    AddVideoButtonを実装 & ホバー時VideoControllerが浮き上がるように

--- app/components/video_controllers.tsx
+++ app/components/video_controllers.tsx
@@ -108,9 +108,9 @@ function RemoveControl({ index }: { index: number }): JSX.Element {
 
     return (
         <div className="control_item remove_control">
-        <button className="control_button remove_button" type="button" onClick={handleClick}>
-            <RemoveIcon />
-        </button>
+            <button className="control_button remove_button" type="button" onClick={handleClick}>
+                <RemoveIcon />
+            </button>
         </div>
     );
 }
@@ -123,6 +123,20 @@ function AddVideoButton(): JSX.Element {
     }
 
     return (
-        <button className="add_video_button" type="button" onClick={handleClick}>+</button>
+        <div className="add_video_button_container">
+            <button className="add_video_button" type="button" onClick={handleClick}>
+                <PlusClipIcon />
+            </button>
+        </div>
     );
+}
+
+
+export function PlusClipIcon() {
+    return <svg width="0" height="0" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <clipPath id="plus_clip_icon" clipPathUnits="objectBoundingBox">
+            <path d="M27.5001 17.0833H17.0834V27.5C17.0834 28.6458 16.1459 29.5833 15.0001 29.5833C13.8542 29.5833 12.9167 28.6458 12.9167 27.5V17.0833H2.50008C1.35425 17.0833 0.416748 16.1458 0.416748 15C0.416748 13.8542 1.35425 12.9167 2.50008 12.9167H12.9167V2.49999C12.9167 1.35416 13.8542 0.416656 15.0001 0.416656C16.1459 0.416656 17.0834 1.35416 17.0834 2.49999V12.9167H27.5001C28.6459 12.9167 29.5834 13.8542 29.5834 15C29.5834 16.1458 28.6459 17.0833 27.5001 17.0833Z"
+                transform="scale(0.033)" />
+        </clipPath>
+    </svg>
 }
