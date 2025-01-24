b03cacaa5d049ea859e9e29cb3185103fd50c43a
Zemelua <zemelua1107@gmail.com>
2025-01-24 21:17:04 +0900

    LayoutOptionsのスタイルを実装

---
+++ app/components/common/icons.tsx
@@ -0,0 +1,84 @@
+export function IconLayoutOption2A(): JSX.Element {
+	return (
+		<svg width="90" height="90" viewBox="0 0 90 90" fill="#757575" xmlns="http://www.w3.org/2000/svg" className="icon">
+			<g filter="url(#filter0_ii_124_170)" >
+				<path d="m 26.25,46.875 a 3.750375,3.750375 0 0 0 -3.75,3.75 V 78.75 a 3.750375,3.750375 0 0 0 3.75,3.75 h 37.5 A 3.750375,3.750375 0 0 0 67.5,78.75 V 50.625 a 3.750375,3.750375 0 0 0 -3.75,-3.75 z m 3.75,7.5 H 60 V 75 H 30 Z" />
+				<path d="m 26.25,7.5 a 3.750375,3.750375 0 0 0 -3.75,3.75 v 28.125 a 3.750375,3.750375 0 0 0 3.75,3.75 h 37.5 a 3.750375,3.750375 0 0 0 3.75,-3.75 V 11.25 A 3.750375,3.750375 0 0 0 63.75,7.5 Z M 30,15 H 60 V 35.625 H 30 Z" />
+			</g>
+			<defs >
+				<filter id="filter0_ii_124_170" x="-7.5" y="-7.5" width="105" height="105" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+					<feFlood floodOpacity="0" result="BackgroundImageFix" id="feFlood2" />
+					<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" id="feBlend2" />
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+						id="feColorMatrix2" />
+					<feOffset
+						dx="-7.5"
+						dy="-7.5"
+						id="feOffset2" />
+					<feGaussianBlur
+						stdDeviation="7.5"
+						id="feGaussianBlur2" />
+					<feComposite
+						in2="hardAlpha"
+						operator="arithmetic"
+						k2="-1"
+						k3="1"
+						id="feComposite2" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.15 0"
+						id="feColorMatrix3" />
+					<feBlend
+						mode="normal"
+						in2="shape"
+						result="effect1_innerShadow_124_170"
+						id="feBlend3" />
+					<feColorMatrix
+						in="SourceAlpha"
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+						result="hardAlpha"
+						id="feColorMatrix4" />
+					<feOffset
+						dx="7.5"
+						dy="7.5"
+						id="feOffset4" />
+					<feGaussianBlur
+						stdDeviation="7.5"
+						id="feGaussianBlur4" />
+					<feComposite
+						in2="hardAlpha"
+						operator="arithmetic"
+						k2="-1"
+						k3="1"
+						id="feComposite4" />
+					<feColorMatrix
+						type="matrix"
+						values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+						id="feColorMatrix5" />
+					<feBlend
+						mode="normal"
+						in2="effect1_innerShadow_124_170"
+						result="effect2_innerShadow_124_170"
+						id="feBlend5" />
+				</filter>
+			</defs>
+		</svg>
+
+	)
+}
+
+export function IconLayoutOption2AClip(): JSX.Element {
+	return (
+		<svg width="0" height="0" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<clipPath id="icon_layout_option_clip" clipPathUnits="objectBoundingBox">
+				<path d="m 26.25,46.875 a 3.750375,3.750375 0 0 0 -3.75,3.75 V 78.75 a 3.750375,3.750375 0 0 0 3.75,3.75 h 37.5 A 3.750375,3.750375 0 0 0 67.5,78.75 V 50.625 a 3.750375,3.750375 0 0 0 -3.75,-3.75 z m 3.75,7.5 H 60 V 75 H 30 Z" transform="scale(0.01111111)"/>
+				<path d="m 26.25,7.5 a 3.750375,3.750375 0 0 0 -3.75,3.75 v 28.125 a 3.750375,3.750375 0 0 0 3.75,3.75 h 37.5 a 3.750375,3.750375 0 0 0 3.75,-3.75 V 11.25 A 3.750375,3.750375 0 0 0 63.75,7.5 Z M 30,15 H 60 V 35.625 H 30 Z" transform="scale(0.01111111)" />
+			</clipPath>
+		</svg>
+	)
+}
--- app/components/layout_options.tsx
+++ app/components/layout_options.tsx
@@ -1,27 +1,39 @@
 import { useContext } from "react";
 import "~/components/layout_options.css"
 import { LayoutContext } from "~/routes/dev.layout_options";
+import { IconLayoutOption2A, IconLayoutOption2AClip } from "./common/icons";
 
 export default function LayoutOptionsContainer() {
     return (
         <div className="layout_options_container">
-            <LayoutOption layoutID={0}></LayoutOption>
-            <LayoutOption layoutID={1}></LayoutOption>
-            <LayoutOption layoutID={2}></LayoutOption>
+            <LayoutOption layoutID={"2A"}></LayoutOption>
+            <LayoutOption layoutID={"2B"}></LayoutOption>
+            <LayoutOption layoutID={"2C"}></LayoutOption>
         </div>
     );
 }
 
-export function LayoutOption({ layoutID }: { layoutID: number }) {
+export function LayoutOption({ layoutID }: { layoutID: string }) {
     const { layout, setLayout } = useContext(LayoutContext);
     const isSelected = layoutID === layout;
 
     return (
         <button
-            className={"layout_option" + (isSelected ? " layout_option_selected" : "")}
+            className={"layout_option" + (isSelected ? " selected" : "")}
             onClick={() => setLayout(layoutID)}
             type="button">
-            <div className="layout_option_placeholder">{layoutID}</div>
+            {getOptionIcon(layoutID)}
         </button>
     );
+}
+
+function getOptionIcon(id: string): [JSX.Element, JSX.Element] {
+    let icons: [JSX.Element, JSX.Element];
+
+    switch (id) {
+        case "2A": icons = [<IconLayoutOption2AClip key={id} />, <IconLayoutOption2A key={id} />]; break;
+        default: icons = [<IconLayoutOption2AClip key={id} />, <IconLayoutOption2A key={id} />];
+    }
+
+    return icons;
 }
