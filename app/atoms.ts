481df9f77997983843117cc6c5766c6f39af05b2
Zemelua <zemelua1107@gmail.com>
2025-04-08 15:22:04 +0900

    コードのリファクタリング

--- app/atoms.ts
+++ app/atoms.ts
@@ -5,17 +5,18 @@ const videoListStorage = createJSONStorage<Video[]>(
     () => localStorage,
     {
         reviver: (_k, value) => {
-            if (typeof value === "object" && value !== null) {
-                switch ((value as { type: string }).type) {
-                    case "VideoTest":
-                        return Object.assign(new VideoTest(false, 0, 0), value);
-                    case "TwitchVideo":
-                        return Object.assign(new TwitchVideo(""), value);
-                    default:
-                        throw new Error("Invalid video type");
-                }
+            if (typeof value !== "object" || value === null) {
+                return value;
             }
-            return value;
+
+            switch ((value as { type: string }).type) {
+                case "VideoTest":
+                    return Object.assign(new VideoTest(false, 0, 0), value);
+                case "TwitchVideo":
+                    return Object.assign(new TwitchVideo(""), value);
+                default:
+                    throw new Error("Invalid video type");
+            }
         }
     }
 );
