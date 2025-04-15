a05bd8c3083bb705c433559980cf644611106eb1
Zemelua <zemelua1107@gmail.com>
2025-04-15 14:17:25 +0900

    [test] _index.tsxにテスト用ボタンを配置

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useSetAtom } from "jotai";
+import { videoDataListAtom } from "~/atoms";
 import { Sidebar } from "~/components/sidebar";
 import { VideoControllersContainer } from "~/components/video_controllers";
 import { VideosContainer } from "~/components/VideosContainer";
@@ -10,11 +12,19 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+    const setVideoList = useSetAtom(videoDataListAtom);
+
     return (
         <main>
             <aside>
                 <Sidebar>
                     <VideoControllersContainer />
+                    {
+                        /* テスト用ボタン。Modalの修正がマージされたら消してね */
+                        <button onClick={() => setVideoList(prev => [...prev, { platform: "twitch", channel: "akamikarubi" }])}>
+                            {"[test] Add twitch video"}
+                        </button>
+                    }
                 </Sidebar>
             </aside>
             <VideosContainer />
