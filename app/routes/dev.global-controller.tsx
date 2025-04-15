bd60dd609b331ea87a766d6af47628931c9fbddc
Zemelua <zemelua1107@gmail.com>
2025-04-15 19:02:00 +0900

    [test] テスト用ページを追加

---
+++ app/routes/dev.global-controller.tsx
@@ -0,0 +1,13 @@
+export default function GlobalController(): JSX.Element {
+	return (
+		<div className="sidebar_placeholder" style={{
+			width: "450px",
+			height: "100%",
+			padding: "30px",
+			border: "1px solid black",
+			background: "#F5F5F5"
+		}}>
+
+		</div>
+	)
+}
