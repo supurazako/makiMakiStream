b274c57bfe1ac979866936c321a013976d49cb31
Zemelua <zemelua1107@gmail.com>
2025-04-20 14:54:01 +0900

    [refactor] Delete unused icon

--- app/components/common/icons.tsx
+++ app/components/common/icons.tsx
@@ -505,34 +505,6 @@ export function CloseIcon(): JSX.Element {
 	);
 }
 
-export function BorderLeft(): JSX.Element {
-	return (
-		<svg width="12" height="114" viewBox="0 0 12 114" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<g filter="url(#filter0_ii_225_282)">
-				<path d="M10 112C2 112 2 104 2 104V10C2 10 2 2 10 2" stroke="#D9D9D9" strokeWidth="3" strokeLinecap="round" />
-			</g>
-			<defs>
-				<filter id="filter0_ii_225_282" x="-1.5" y="-1.5" width="15" height="117" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-					<feFlood floodOpacity="0" result="BackgroundImageFix" />
-					<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-					<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-					<feOffset dx="-2" dy="-2" />
-					<feGaussianBlur stdDeviation="2" />
-					<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-					<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.15 0" />
-					<feBlend mode="normal" in2="shape" result="effect1_innerShadow_225_282" />
-					<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-					<feOffset dx="2" dy="2" />
-					<feGaussianBlur stdDeviation="2" />
-					<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
-					<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-					<feBlend mode="normal" in2="effect1_innerShadow_225_282" result="effect2_innerShadow_225_282" />
-				</filter>
-			</defs>
-		</svg>
-	)
-}
-
 export function ArrowDownIcon(): JSX.Element {
 	return (
 		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
