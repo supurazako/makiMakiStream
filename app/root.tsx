a25bbfab19da977aa949e77416cda804b7435955
supurazako <redhot30atama@gmail.com>
2025-05-16 21:33:27 +0900

    [refactor] formatt import statement

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,10 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import {
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration
+} from "react-router";
 import type { LinksFunction } from "react-router";
 
 export const links: LinksFunction = () => [
