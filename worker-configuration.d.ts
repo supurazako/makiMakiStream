1087e3477e39e9b1536a35ab74523a195521f76a
supurazako <redhot30atama@gmail.com>
2025-07-22 11:10:52 +0900

    fix(eslint ignore) eslintの無効化の文を削除 なぜ必要かはわからないので、問題が起きたらごめん

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -3794,7 +3794,7 @@ type AIGatewayHeaders = {
     [key: string]: string | number | boolean | object;
 };
 type AIGatewayUniversalRequest = {
-    provider: AIGatewayProviders | string; // eslint-disable-line
+    provider: AIGatewayProviders | string;
     endpoint: string;
     headers: Partial<AIGatewayHeaders>;
     query: unknown;
@@ -3810,7 +3810,7 @@ declare abstract class AiGateway {
         gateway?: GatewayOptions;
         extraHeaders?: object;
     }): Promise<Response>;
-    getUrl(provider?: AIGatewayProviders | string): Promise<string>; // eslint-disable-line
+    getUrl(provider?: AIGatewayProviders | string): Promise<string>;
 }
 interface AutoRAGInternalError extends Error {
 }
