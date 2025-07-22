c5691c2da58a3fb05faa188eb3063bfcb3d114d2
supurazako <redhot30atama@gmail.com>
2025-07-22 11:10:23 +0900

    feat(eslint) eslintの設定を更新した

---
+++ eslint.config.mjs
@@ -0,0 +1,108 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { defineConfig, globalIgnores } from "eslint/config";
+
+import globals from "globals";
+
+import { fixupConfigRules } from "@eslint/compat";
+
+import tsParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+
+        globals: {
+            ...globals.browser,
+            ...globals.commonjs,
+        },
+    },
+
+    extends: compat.extends("eslint:recommended"),
+}, globalIgnores(["!**/.server", "!**/.client", "build/", ".react-router/", ".wrangler/", "@types/", "node_modules/", "public/"]), {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+
+    plugins: {
+    },
+
+    extends: fixupConfigRules(compat.extends(
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended",
+    )),
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+
+        formComponents: ["Form"],
+
+        linkComponents: [{
+            name: "Link",
+            linkAttribute: "to",
+        }, {
+            name: "NavLink",
+            linkAttribute: "to",
+        }],
+
+        "import/resolver": {
+            typescript: {},
+        },
+    },
+}, {
+    files: ["**/*.{ts,tsx}"],
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    settings: {
+        "import/internal-regex": "^~/",
+
+        "import/resolver": {
+            node: {
+                extensions: [".ts", ".tsx"],
+            },
+
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+    },
+
+    extends: fixupConfigRules(compat.extends(
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+    )),
+}, {
+    files: ["**/.eslintrc.cjs"],
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+    },
+}]);
